f03691578a6a29f5cc937794b777c814270e116e
Eldar <eldaryergengali@gmail.com>
2024-07-30 12:33:14 +0500

    fixing bag

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
-if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
